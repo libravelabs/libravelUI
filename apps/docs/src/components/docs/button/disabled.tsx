5990b44a70d39fd99b12d3f8c1f3757f02bd5214
bimaalbertus <bimaalbertus13@gmail.com>
2025-10-27 17:51:26 +0700

    minor update: fix bug

--- apps/docs/src/components/docs/button/disabled.tsx
+++ apps/docs/src/components/docs/button/disabled.tsx
@@ -3,5 +3,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function DisabledButton() {
-  return <Button isDisabled>I'm just here for show</Button>;
+  return <Button isDisabled>I&apos;m just here for show</Button>;
 }
