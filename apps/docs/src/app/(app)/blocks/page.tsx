6feba350ae3d1cf8f7abcebf9b05857adb5354de
libravelabs <libravelabs@gmail.com>
2026-03-27 23:45:41 +0700

    latest update

--- apps/docs/src/app/(app)/blocks/page.tsx
+++
@@ -1,9 +0,0 @@
-import { BlockPlayground } from "@/components/docs/block-playground";
-
-export default function Page() {
-  return (
-    <div>
-      <BlockPlayground src="/preview/navbar/1" title="Navbar Default" />
-    </div>
-  );
-}
